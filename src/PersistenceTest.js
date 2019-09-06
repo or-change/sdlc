0d6ffccc19ffae022fad7c634a84e114e293285f
leoword <leowordpassword123>
2019-09-06 16:25:46 +0800

    [ADDED] the model and persistence test

---
+++ src/PersistenceTest.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const assert = require('assert');
+
+module.exports = function (store) {
+	describe('Persistence Testing', () => {
+		describe('Account Testing', () => {
+			it('test', () => {
+				assert.ok();
+			});
+		});
+
+		describe('Project Testing', () => {
+
+		});
+
+		describe('Version Testing', () => {
+
+		});
+
+		describe('Member Testing', () => {
+
+		});
+
+		describe('Flow Testing', () => {
+
+		});
+
+		describe('Trace Testing', () => {
+
+		});
+	});
+};
--- test/model.spec.js
+++ test/model.spec.js
@@ -1,5 +1,86 @@
-// 测试持久化工厂，生成持久化测试，model测试的时候生成持久化测试实例，以便以后的持久化实现可以使用该测试工厂
+'use strict';
 
-describe('', () => {
+const assert = require('assert');
 
+const Datahub = require('@or-change/data-hub');
+const models = [
+	require('../src/models/Account'),
+	require('../src/models/Project'),
+	require('../src/models/Version'),
+	require('../src/models/Flow'),  
+	require('../src/models/Trace')
+].reduce((all, group) => Object.assign(all, group), {});
+const store = require('../example/persistence')();
+
+const PersistenceTest = require('../src/PersistenceTest');
+
+const Model = Datahub.create({
+	id: 'test',
+	models: (() => {
+		const modelOptions = {};
+
+		for (let key in models) {
+			modelOptions[key] = Object.assign({}, models[key](store), {
+				symbol: key
+			});
+		}
+
+		return modelOptions;
+	})()
+}).model;
+
+describe('Testing', () => {
+	PersistenceTest(store);
+
+	describe('Model Testing', () => {
+		describe('Account Testing', () => {
+			it('test', () => {
+				assert.ok();
+			});
+		});
+
+		describe('AccountList Testing', () => {
+
+		});
+
+		describe('Project Testing', () => {
+
+		});
+
+		describe('ProjectList Testing', () => {
+
+		});
+
+		describe('Version Testing', () => {
+
+		});
+
+		describe('VersionList Testing', () => {
+
+		});
+
+		describe('Member Testing', () => {
+
+		});
+
+		describe('MemberList Testing', () => {
+
+		});
+
+		describe('Flow Testing', () => {
+
+		});
+
+		describe('FlowList Testing', () => {
+
+		});
+
+		describe('Trace Testing', () => {
+
+		});
+
+		describe('TraceList Testing', () => {
+
+		});
+	});
 });
